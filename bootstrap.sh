aac5004f0167819c78593eee75428c83a31e9d99
tx-zero <86655896+tx-zero@users.noreply.github.com>
2022-05-26 14:41:10 +1000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 mkdir -p /tmp/dotnet && curl -fsSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 6.0 --install-dir /tmp/dotnet
 dotnet dev-certs https
-echo "export DOTNET_ROOT=/tmp/dotnet" >> /etc/profile
-echo "export PATH=$PATH:/tmp/dotnet" >> /etc/profile
+echo "export DOTNET_ROOT=/tmp/dotnet" >> ~/.profile
+echo "export PATH=$PATH:/tmp/dotnet" >> ~/.profile
